02104dcde49b4b6ad1c19ad4f021d4e5b3efafd0
najmadm <najma.216@gmail.com>
2026-02-09 01:48:36 +0000

    HW

---
+++ najma_mohamed.py
@@ -0,0 +1,23 @@
+# List Exercise
+
+items = ["pie", "chocolate milk", "mango", "fries", "pasta", "rice"]
+
+print(items)
+
+print("The first two items in the list are:", items[0], ",", items[1])
+print("The middle two items in the list are:", items[2], ",", items[3])
+print("The first and last items in the list are:", items[0], ",", items[5])
+
+
+# Tuple Exercise
+
+menu = ("pie", "fries", "pasta", "mango", "rice")
+
+for food in menu:
+    print(food)
+
+# Updated menu (replace two foods)
+menu = ("pie", "burger", "pasta", "salad", "rice")
+
+for food in menu:
+    print(food)
